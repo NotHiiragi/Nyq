e7af3e24754a84dbb74d6244efbe70942ca78107
Vilmer Johansson <47473249+WaffleTheCool@users.noreply.github.com>
2022-03-23 18:16:03 +0100

    Update ModConfig.hpp

--- include/ModConfig.hpp
+++ include/ModConfig.hpp
@@ -1,8 +1,8 @@
 #include "config-utils/shared/config-utils.hpp"
 DECLARE_CONFIG(ModConfig,
- CONFIG_VALUE(Active, bool, "NSFW", False);
+ CONFIG_VALUE(NSFW, bool, "Only NSFW?", False);
 
  CONFIG_INIT_FUNCTION(
- CONFIG_INIT_VALUE(Active);
+ CONFIG_INIT_VALUE(NSFW);
  )
 )
